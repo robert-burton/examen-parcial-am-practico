90bc8eadf06d9cb30a14984385371aafaf5c9d9c
Roberto Mora <rmora92911@gmail.com>
2020-10-07 20:46:39 -0500

    primer commit

---
+++ ApiWeatherCall.js
@@ -0,0 +1,5 @@
+//Este archivo se encarga de hacer la llamada AJAX y traer la info
+
+//El resultado se pasa a dataViewer
+
+//Debe tener la logica de mostrar/ocultar elementos e info
---
+++ DataViewer.js
@@ -0,0 +1,1 @@
+//Creo que este se encarga de darle formato a las cosas, no sé
