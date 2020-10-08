321ff272ee92dbb507ca1fec3e77cdcd87400444
robert-burton <rmora92911@gmail.com>
2020-10-08 02:41:59 -0500

    segundo commit

--- ApiWeatherCall.js
+++ ApiWeatherCall.js
@@ -3,3 +3,42 @@
 //El resultado se pasa a dataViewer
 
 //Debe tener la logica de mostrar/ocultar elementos e info
+
+var citySearched = document.getElementById("ciudadIn").value;
+
+function search() {
+  var citySearched = document.getElementById("ciudadIn").value;
+  document.getElementById("searchButtonDiv").style.display = "none";
+  if (citySearched == "") {
+    alert("Hay que ingresar una ciudad");
+  } else {
+    var apiURL =
+      "api.openweathermap.org/data/2.5/weather?q=" +
+      citySearched +
+      "&appid=4c1f52b065baf1561a6fe124b58b84d6";
+    getApiData(apiURL);
+  }
+  document.getElementById("searchButtonDiv").style.display = "inline";
+}
+
+//Esta fn llama a la API y pasarla a dataViewer
+function getApiData(apiURL) {
+  var request = new XMLHttpRequest();
+  request.open("GET", apiURL, true);
+  request.send();
+
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = this.response;
+      var myData = JSON.parse(data);
+
+      var info = {
+        cityName: myData.name,
+        currentTemp: myData.main.temp,
+        maxTemp: myData.main.temp_max,
+        minTemp: myData.main.temp_min,
+      };
+      showCityWeather(info);
+    }
+  };
+}
--- DataViewer.js
+++ DataViewer.js
@@ -1,1 +1,27 @@
-//Creo que este se encarga de darle formato a las cosas, no sé
+//Da formato a la info
+
+function showCityWeather(info) {
+  document.getElementById("result").innerHTML = "";
+  var elementHeader = document.createElement("h5");
+  document.getElementById("result").appendChild(elementHeader);
+
+  var display = document.createElement("h4");
+  display.style.color = "white";
+  display.innerHTML = info.cityName + "<br ><hr />";
+  document.getElementById("result").appendChild(display);
+
+  var display = document.createElement("h4");
+  display.style.color = "white";
+  display.innerHTML = info.currentTemp + "°C actualmente<br ><hr />";
+  document.getElementById("result").appendChild(display);
+
+  var display = document.createElement("h4");
+  display.style.color = "white";
+  display.innerHTML = info.maxTemp + "°C max<br ><hr />";
+  document.getElementById("result").appendChild(display);
+
+  var display = document.createElement("h4");
+  display.style.color = "white";
+  display.innerHTML = info.minTemp + "°C min<br ><hr />";
+  document.getElementById("result").appendChild(display);
+}
