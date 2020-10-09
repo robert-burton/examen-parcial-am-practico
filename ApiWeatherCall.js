b0f2370463d770154a9e135ff53e55a0ba38797e
robert-burton <rmora92911@gmail.com>
2020-10-08 21:54:27 -0500

    commit final

--- ApiWeatherCall.js
+++ ApiWeatherCall.js
@@ -1,11 +1,6 @@
-//Este archivo se encarga de hacer la llamada AJAX y traer la info
-
-//El resultado se pasa a dataViewer
-
-//Debe tener la logica de mostrar/ocultar elementos e info
-
 var citySearched = document.getElementById("ciudadIn").value;
 
+//Valida que haya una entrada de texto; si se valida, se llama a la API
 function search() {
   var citySearched = document.getElementById("ciudadIn").value;
   document.getElementById("searchButtonDiv").style.display = "none";
@@ -13,7 +8,7 @@ function search() {
     alert("Hay que ingresar una ciudad");
   } else {
     var apiURL =
-      "api.openweathermap.org/data/2.5/weather?q=" +
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
       citySearched +
       "&appid=4c1f52b065baf1561a6fe124b58b84d6";
     getApiData(apiURL);
@@ -21,24 +16,49 @@ function search() {
   document.getElementById("searchButtonDiv").style.display = "inline";
 }
 
-//Esta fn llama a la API y pasarla a dataViewer
+//Llama a la API y pasa info tratada a dataViewer
 function getApiData(apiURL) {
+
   var request = new XMLHttpRequest();
   request.open("GET", apiURL, true);
   request.send();
 
   request.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+    if(this.readyState == 4 && this.status == 404){
+      alert("Ciudad no encontrada");
+    }
+    else if (this.readyState == 4 && this.status == 200) {
       var data = this.response;
       var myData = JSON.parse(data);
-
-      var info = {
-        cityName: myData.name,
-        currentTemp: myData.main.temp,
-        maxTemp: myData.main.temp_max,
-        minTemp: myData.main.temp_min,
-      };
-      showCityWeather(info);
+      treatData(myData);
     }
+  }
+}
+
+//Da tratamiento y valida los datos de la ciudad buscada
+function treatData(myData){
+  var info = {
+    cityName: myData.name,
+    currentTemp: myData.main.temp-273.15,
+    maxTemp: myData.main.temp_max-273.15,
+    minTemp: myData.main.temp_min-273.15,
+    country: myData.sys.country,
   };
+
+  //Comprobar que ciudad este en MX
+  if(info.country != "MX"){
+    alert(info.cityName + " no se encuentra en Mexico");
+  }
+  else{
+    roundTempToTwoDecimals(info);
+    showCityWeather(info);
+  }
+}
+
+
+//Rendondea temperaturas a dos decimales
+function roundTempToTwoDecimals(info){
+  info.currentTemp = info.currentTemp.toFixed(2);
+  info.maxTemp = info.maxTemp.toFixed(2);
+  info.minTemp = info.minTemp.toFixed(2);
 }
--- DataViewer.js
+++ DataViewer.js
@@ -1,27 +1,51 @@
-//Da formato a la info
+/*FUENTES CONSULTADAS
+
+Como redondear un numero a dos decimales: https://www.w3schools.com/jsref/jsref_tofixed.asp
+
+*/
 
 function showCityWeather(info) {
   document.getElementById("result").innerHTML = "";
   var elementHeader = document.createElement("h5");
   document.getElementById("result").appendChild(elementHeader);
 
-  var display = document.createElement("h4");
-  display.style.color = "white";
-  display.innerHTML = info.cityName + "<br ><hr />";
+  //Nombre de ciudad
+  var display = document.createElement("h1");
+  display.style.color = "whitesmoke";
+  display.innerHTML = info.cityName + "<br >";
   document.getElementById("result").appendChild(display);
 
-  var display = document.createElement("h4");
-  display.style.color = "white";
-  display.innerHTML = info.currentTemp + "°C actualmente<br ><hr />";
-  document.getElementById("result").appendChild(display);
+  //Temperaturas
+  createTempSection(info,"current");
+  createTempSection(info,"max");
+  createTempSection(info,"min");
+}
 
-  var display = document.createElement("h4");
-  display.style.color = "white";
-  display.innerHTML = info.maxTemp + "°C max<br ><hr />";
-  document.getElementById("result").appendChild(display);
+//Crea las impresiones de temperatura, dependiendo del tipo
+function createTempSection(info, tempType){
+  if(tempType == "current"){
+    temp = info.currentTemp;
+    textColor = "green";
+    infoPrint = "°C actualmente<br >";
+    flag = 1;
+  }
+  if(tempType == "max"){
+    temp = info.maxTemp;
+    textColor = "red";
+    infoPrint = "°C max<br >";
+    flag = 1;
+  }
+  if(tempType == "min"){
+    temp = info.minTemp;
+    textColor = "cyan";
+    infoPrint = "°C min<br >";
+    flag = 1;
+  }
+  if(flag == 1){
+    display = document.createElement("h2");
+    display.style.color = textColor;
+    display.innerHTML = temp + infoPrint;
+    document.getElementById("result").appendChild(display);
+  }
 
-  var display = document.createElement("h4");
-  display.style.color = "white";
-  display.innerHTML = info.minTemp + "°C min<br ><hr />";
-  document.getElementById("result").appendChild(display);
 }
